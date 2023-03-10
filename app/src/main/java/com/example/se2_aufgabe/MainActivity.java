30152114b07639b8911455aeda0d9ae090f39452
pg98gh <phigattenloehner@outlook.com>
2023-03-10 14:45:38 +0100

    working server connection

--- app/src/main/java/com/example/se2_aufgabe/MainActivity.java
+++ app/src/main/java/com/example/se2_aufgabe/MainActivity.java
@@ -19,14 +19,16 @@ public class MainActivity extends AppCompatActivity {
     private Button sendtoserver;
     private Connection net;
     private EditText editText;
-    private Button calc;
+    private Button number;
     private TextView server;
-    private EditText result;
+    private EditText final_mtrnr;
     private EditText mtrnr;
 
     //mtr ==01613210 %7=0
     @SuppressLint("WrongViewCast")
     public void calculateNumber(View view){
+
+
 
     }
 
@@ -36,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view=null;
+        setContentView(R.layout.activity_main);
+        editText= findViewById(R.id.mtrnr);
+        sendtoserver=findViewById(R.id.calculate);
+        sendtoserver.setOnClickListener(
+                a ->{
+
+                    net = new Connection(editText.getText().toString());
+                    net.start();
+                    try{
+                        net.join();
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    Log.i("info", String.valueOf(editText));
+                    server = findViewById(R.id.server);
+                    server.setText(net.getResult());
+
+
+                }
+
+        );
+
 
     }
 
